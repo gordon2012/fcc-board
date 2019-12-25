0b346e5eefb9fcc547a161324e461e951631d2ae
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-12-25 00:48:23 -0700

    remove debug and update now deploy
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BASE_URL } from './index';
 
 import Layout from './components/Layout';
 import Card from './components/Card';
-import Code from './components/Code';
 import Form from './components/Form';
 import Input from './components/Input';
 import Button from './components/Button';
@@ -35,22 +34,6 @@ const List = styled.ul``;
 const boards = ['general', 'advanced', 'specialized'];
 
 const App = () => {
-    const [responses, setResponses] = React.useState([]);
-
-    const [results, setResults] = React.useState({});
-    const clearResult = name =>
-        setResults(prevState => {
-            const { [name]: __, ...newState } = prevState;
-            return newState;
-        });
-
-    const getExample = async () => {
-        const response = await fetch(`${BASE_URL}/api/example`);
-        const data = await response.json();
-        setResponses(prevState => [data, ...prevState]);
-        setResults(prevState => ({ ...prevState, getExample: data }));
-    };
-
     const [previews, setPreviews] = React.useState({});
     const [deleted, setDeleted] = React.useState({});
     const [reported, setReported] = React.useState({});
@@ -152,29 +135,28 @@ const App = () => {
         <>
             <GlobalStyle />
             <Layout>
-                <Title>
-                    Information Security and Quality Assurance Boilerplate
-                </Title>
+                <Title>Anonymous Message Board</Title>
 
                 <Card>
                     <h3>User Stories</h3>
-                    <List as="ol">
-                        <li>I will have user stories.</li>
+                    <List>
+                        {[
+                            `Only allow your site to be loading in an iFrame on your own pages.`,
+                            `Do not allow DNS prefetching.`,
+                            `Only allow your site to send the referrer for your own pages.`,
+                            `I can POST a thread to a specific message board by passing form data text and deletepassword_ to /api/threads/{board}. Saved will be at least _id, text, createdon_(date&time), bumpedon_(date&time, starts same as created_on), reported(boolean), deletepassword_, & replies(array).`,
+                            `I can POST a reply to a thread on a specific board by passing form data text, deletepassword_, & threadid_ to /api/replies/{board} and it will also update the bumped_on date to the comments date. In the thread's replies array will be saved _id, text, createdon_, deletepassword_, & reported.`,
+                            `I can GET an array of the most recent 10 bumped threads on the board with only the most recent 3 replies each from /api/threads/{board}. The reported and deletepasswords_ fields will not be sent to the client.`,
+                            `I can GET an entire thread with all its replies from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields the client should be see.`,
+                            `I can delete a thread completely if I send a DELETE request to /api/threads/{board} and pass along the threadid_ & deletepassword_. (Text response will be 'incorrect password' or 'success')`,
+                            `I can delete a post(just changing the text to '[deleted]' instead of removing completely like a thread) if I send a DELETE request to /api/replies/{board} and pass along the threadid_, replyid_, & deletepassword_. (Text response will be 'incorrect password' or 'success')`,
+                            `I can report a thread and change its reported value to true by sending a PUT request to /api/threads/{board} and pass along the threadid_. (Text response will be 'success')`,
+                            `I can report a reply and change its reported value to true by sending a PUT request to /api/replies/{board} and pass along the threadid_ & replyid_. (Text response will be 'success')`,
+                            `Complete functional tests that wholly test routes and pass.`,
+                        ].map((e, i) => (
+                            <li key={i}>{e}</li>
+                        ))}
                     </List>
-                </Card>
-
-                <Card>
-                    <h3>Example Usage</h3>
-                    <Code>/api/test</Code>
-                </Card>
-
-                <Card>
-                    <h3>Example Return</h3>
-                    <Code box>
-                        {{
-                            hello: 'world',
-                        }}
-                    </Code>
                 </Card>
 
                 <Title as="h2">Front-End</Title>
@@ -314,25 +296,6 @@ const App = () => {
                         )}
                     </Card>
                 ))}
-
-                <Card>
-                    <h3>Debug</h3>
-
-                    <Code box>{previews}</Code>
-                </Card>
-
-                {responses.length > 0 && (
-                    <>
-                        <Title as="h2">Responses</Title>
-                        <Card>
-                            {responses.map((e, i) => (
-                                <Code box key={i}>
-                                    {e}
-                                </Code>
-                            ))}
-                        </Card>
-                    </>
-                )}
             </Layout>
         </>
     );
